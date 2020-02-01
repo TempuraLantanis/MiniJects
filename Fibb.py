450f9cce8845f50c9a80e60f4433c4d35728baed
kArdetL <33734673+Zionix110@users.noreply.github.com>
2020-02-01 13:44:10 -0300

    Create Fibb.py

---
+++ Fibb.py
@@ -0,0 +1,25 @@
+def fibonacci(n):
+  if(n<3):
+    return 1 #ensures base case
+  else:
+    a = n
+    lamp = range(3,a) # calling iteration to the nth number
+    c = 1
+    for s in lamp:
+      if c != 1:
+        f = d + c
+        d = c
+        c = f
+        print("endres"+" "+ str(f))
+      else:
+        d = c + c
+        e = d + c
+        c = e
+        
+        print("C is"+ " "+ str(c))
+        print("D is"+ " "+ str(d))
+      
+        
+
+
+fibonacci(25)
